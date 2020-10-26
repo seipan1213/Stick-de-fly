99b9ad315a0b3793d5c7423ba197aea5b7e5c278
nekokojpn <nekokojpn@gmail.com>
2020-10-26 14:01:23 +0900

    Add initial scene

---
+++ Assets/Scripts/Gs2/TitleScene/CredentialDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CredentialDirector : MonoBehaviour
+{
+    [SerializeField]
+    CredentialSettings _credentialSettings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Gs2/TitleScene/CredentialSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CredentialSettings : MonoBehaviour
+{
+    //バックエンドサーバに接続するための識別子
+    [SerializeField]
+    public string applicationCliendId;
+    [SerializeField]
+    public string applicationClientSecret;
+}
