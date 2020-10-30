cfe46ac6f31cee910421256d2c35450588fb11c4
seipan1213 <seiju1213@gmail.com>
2020-10-31 01:29:14 +0900

    DataManager
    DataManagerのオブジェクト作って
    値を保持できるようにした

---
+++ Assets/MyGame/script/DataManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int diamond = 0;
+    public int coin = 0;
+
+    public string user_name = "???";
+    public int now_char;
+    public bool[] is_char;
+    void Start()
+    {
+
+    }
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/MyGame/script/TitleManager.cs
+++ Assets/MyGame/script/TitleManager.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class TitleManager : MonoBehaviour
 {
 
     // Start is called before the first frame update
+    [SerializeField] public GameObject Shop_window;
+
+    public GameObject[] char_item;
+    public GameObject[] diamond;
+
+    Canvas canvas;
+
+    [SerializeField] Text    dia_t;
+    [SerializeField] Text    coin_t;
+    [SerializeField] Text    user_name_t;
+
+    [SerializeField] GameObject data_go;
+    DataManager dataManager;
+
     void Start()
     {
+        if (!GameObject.Find("DataManager"))
+            Instantiate(data_go).name = "DataManager";
+        dataManager = GameObject.Find("DataManager").GetComponent<DataManager>();
 
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        char_item = GameObject.FindGameObjectsWithTag("char_item");
+        diamond = GameObject.FindGameObjectsWithTag("diamond");
+        Shop_window.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        dia_t.text = dataManager.diamond.ToString();
+        coin_t.text = dataManager.coin.ToString();
+        user_name_t.text = dataManager.user_name;
     }
 
     public void Push_1P(){
@@ -25,5 +49,29 @@ public class TitleManager : MonoBehaviour
     }
     public void Push_Options(){
 
+    }
+
+    public void Push_Shop(){
+        Shop_window.SetActive(true);
+        foreach (var item in char_item)
+            item.SetActive(true);
+        foreach (var item in diamond)
+            item.SetActive(false);
+    }
+    public void Close_Shop(){
+        Shop_window.SetActive(false);
+    }
+
+    public void Push_char(){
+        foreach (var item in char_item)
+            item.SetActive(true);
+        foreach (var item in diamond)
+            item.SetActive(false);
+    }
+    public void Push_diamond(){
+        foreach (var item in char_item)
+            item.SetActive(false);
+        foreach (var item in diamond)
+            item.SetActive(true);
     }
 }
